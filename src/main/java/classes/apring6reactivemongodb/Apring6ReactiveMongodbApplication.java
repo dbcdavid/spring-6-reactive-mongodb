9b5a076cd7542c61edb1e2443c2763648a264659
David <dbcdavid.clk@gmail.com>
2024-12-03 18:03:49 -0300

    Initial commit

---
+++ src/main/java/classes/apring6reactivemongodb/Apring6ReactiveMongodbApplication.java
@@ -0,0 +1,13 @@
+package classes.apring6reactivemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apring6ReactiveMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apring6ReactiveMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/classes/apring6reactivemongodb/Apring6ReactiveMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package classes.apring6reactivemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apring6ReactiveMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
